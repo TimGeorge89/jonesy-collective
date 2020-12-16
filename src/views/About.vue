b392a031be6f32d4251e17d6a1e94107c025e7cb
Tim <timdev89@gmail.com>
2020-12-15 22:22:08 -0500

    "our team" v2

--- src/views/About.vue
+++ src/views/About.vue
@@ -22,20 +22,9 @@
             </div>
             <div class="b-content">
               <h3>About Us</h3>
-                <p>Alex Hwang Jones was a young, visionary leader in the Central Florida
-                  electronic arts and dance community. Alex had a huge love of the arts in
-                  every form, as well as deep plant knowledge and strong values for the careful
-                  use of plant medicines - values he widely espoused with his peers, many of whom
-                  are young adults alienated in today’s difficult economy, who are suffering
-                  from stress, depression, and are looking for safe creative outlets that inspire
-                  hope. As his family mourned Alex’s passing this past summer from gun violence, 
-                  his hundreds of friends came to me with thousands of dollars they raised through
-                  a grass-roots effort to memorialize his life. Together we will make a difference 
-                  in Central Florida by connecting to his community, providing important education 
-                  and peer-awareness for harm reduction – through the arts. The Jonesy Collective 
-                  was founded on Oct. 8, 2020. Local community art events are being planned where 
-                  proceeds will be given to more mature national organizations such as MAPs and 
-                  DanceSafe - organizations that reflect Alex’s goals and values
+                <p>Alex Hwang Jones was a young, visionary leader in the Central Florida electronic arts and dance community. Alex’s passing on June 24, 2020 from an armed robbery is deeply mourned by his hundreds of friends, These friends, his sangha, immediately came together,  raising thousands of dollars in a spontaneous grass-roots effort to memorialize his life.    And telling me stories of their friendship.
+                   So I, his mother, in my grief listened to these stories.  Some I knew - that Alex loved the arts in every form, had strong values for the careful use of plant medicines – and had a loving, wise and kind nature.  And I was deeply moved - the stories became countless stories of how he helped, how he impacted so many lives for the better, in inspiration and friendship.  His deep connection with his friends as family was what he lived for - many of whom are tremendous creatives suffering from stress, depression, tough times - and yet with Alex were always looking for ways to help with hope, and strength and doing.   
+                   I am so blessed I do not grieve alone.   We will never forget the light that was my son, their friend and brother, Alex Hwang Jones.  In powerful, loving tribute, the Jonesy Collective was founded on Oct. 8, 2020. Together we will make a difference in Central Florida by connecting to the community, providing important education and awareness for harm reduction, finding nurturing ways to help each other and Mother Earth – and inspiring through the arts. Community art, music and film events are being planned where proceeds will be given to national organizations such as MAPs and DanceSafe, as well a local organizations such as Zebra Coalition and iDignity.  This list is just a beginning as we find ways to help that reflect Alex’s work, his wisdom, goals and values. 
                 </p>
               <div class="b-buttons">
                 <a href="https://www.facebook.com/thejonesycollective" target="_blank" ><button class="button-2">Facebook</button></a>
@@ -57,59 +46,15 @@
       </div>
 
     <!-- FOUNDERS -->
-    <!-- <div class="founders">
+    <div class="founders">
       <div class="team-title">
           <h1>Our Amazing Team</h1>
         </div>
-      <div class="wrapper"> -->
+      <div class="wrapper">
         <!-- CARDS -->
-        <!-- <div class="team-card">
+        <div class="team-card">
           <div>
-            <img src="images/woman.jpg">
-          </div>
-          <div class="team-content">
-            <h3>Mimi Hwang</h3>
-            <p>Mimi Hwang is the founder and chair of the Jonesy Foundation,
-               created in loving tribute to her son Alex Hwang Jones. Mimi is
-               an artist with residency at McRae Arts and Beaux Arts, two of 
-               the largest artist collectives in Central Florida. In her professional
-               life, Mimi spent decades in publishing and media in various executive 
-               roles at Time Inc. (Time Warner) and HBO in NYC, and at Harcourt after 
-               moving to Orlando, FL. Mimi also founded Kao eTechnologies, a technology 
-               consulting firm in Orlando. Mimi is active in her local community serving 
-               on the Timucua Arts Board as Secretary, and the Orange County Democratic 
-               Party as Precinct Woman. Mimi attended University of Wisconsin-Madison 
-               with a degree in Psychology. Mimi currently resides in Winter Park, Fl.
-            </p>
-          </div>
-        </div> -->
-
-        <!-- CARDS -->
-        <!-- <div class="team-card">
-          <div>
-            <img src="images/male.jpg">
-          </div>
-          <div class="team-content">
-            <h3>Mimi Hwang</h3>
-            <p>Mimi Hwang is the founder and chair of the Jonesy Foundation,
-               created in loving tribute to her son Alex Hwang Jones. Mimi is
-               an artist with residency at McRae Arts and Beaux Arts, two of 
-               the largest artist collectives in Central Florida. In her professional
-               life, Mimi spent decades in publishing and media in various executive 
-               roles at Time Inc. (Time Warner) and HBO in NYC, and at Harcourt after 
-               moving to Orlando, FL. Mimi also founded Kao eTechnologies, a technology 
-               consulting firm in Orlando. Mimi is active in her local community serving 
-               on the Timucua Arts Board as Secretary, and the Orange County Democratic 
-               Party as Precinct Woman. Mimi attended University of Wisconsin-Madison 
-               with a degree in Psychology. Mimi currently resides in Winter Park, Fl.
-            </p>
-          </div>
-        </div> -->
-
-        <!-- CARDS -->
-        <!-- <div class="team-card">
-          <div>
-            <img src="images/woman.jpg">
+            <img src="images/head_shot.jpg">
           </div>
           <div class="team-content">
             <h3>Mimi Hwang</h3>
@@ -127,9 +72,52 @@
             </p>
           </div>
         </div>
-      </div>
-    </div> -->
 
+        <!-- CARDS -->
+        <div class="team-card">
+          <!-- <div>
+            <img src="images/male.jpg">
+          </div> -->
+          <div class="team-content">
+            <h1>The Collective</h1>
+            <!-- team member -->
+            <h3>Steve Dykes</h3>
+            <p>Treasurer</p>
+            <!-- team member -->
+            <h3>Kaylee Oberfield</h3>
+            <p>Secretary & Art Task Force</p>
+            <!-- team member -->
+            <h3>Meza Ayala</h3>
+            <p>Art Task Force</p>
+            <!-- team member -->
+            <h3>Tyler Cortez</h3>
+            <p>Art Task Force</p>
+          </div>
+        </div>
+
+        <!-- CARDS -->
+        <!-- <div class="team-card">
+          <div>
+            <img src="images/woman.jpg">
+          </div>
+          <div class="team-content">
+            <h3>Mimi Hwang</h3>
+            <p>Mimi Hwang is the founder and chair of the Jonesy Foundation,
+               created in loving tribute to her son Alex Hwang Jones. Mimi is
+               an artist with residency at McRae Arts and Beaux Arts, two of 
+               the largest artist collectives in Central Florida. In her professional
+               life, Mimi spent decades in publishing and media in various executive 
+               roles at Time Inc. (Time Warner) and HBO in NYC, and at Harcourt after 
+               moving to Orlando, FL. Mimi also founded Kao eTechnologies, a technology 
+               consulting firm in Orlando. Mimi is active in her local community serving 
+               on the Timucua Arts Board as Secretary, and the Orange County Democratic 
+               Party as Precinct Woman. Mimi attended University of Wisconsin-Madison 
+               with a degree in Psychology. Mimi currently resides in Winter Park, Fl.
+            </p>
+          </div>
+        </div> -->
+      </div>
+    </div>
   </div>
 </template>
 
@@ -192,14 +180,17 @@
         padding: 20px;
       }
     }
+    .image-container {
+      margin: auto;
+      img {
+        height: auto;
+        padding-right: 30px;
+        border-radius: 15px;
+        height: 350px;
+      }
+    }
 
-    img {
-      height: auto;
-      padding-right: 30px;
-      border-radius: 15px;
-      height: 350px;
-
-     }
+    
   }
 
   @media screen and (max-width: 1105px) {
@@ -271,14 +262,15 @@
     }  
 
     .wrapper {
-      display: flex;
+      display: block;
       justify-content: space-between;
 
       .team-card {
-      width: 31%;
+      width: 90%;
       background-color: white;
+      margin: 40px 20px;
       align-items: center;
-      padding: 30px 20px;
+      padding: 30px 50px;
       border-radius: 10px;
       box-shadow: 5px 10px 15px rgba(#333, 0.5);
 
@@ -290,26 +282,12 @@
 
       .team-content {
         text-align: center;
+        h1 {
+          margin-bottom: 20px;
+          text-decoration: underline;
+        }
       }
     } 
   }
 }
-
-@media screen and (max-width: 1105px) {
-  .founders {
-        
-    .wrapper {
-      display: block;
-
-      .team-card {
-        width: 90%;
-        margin: 40px 20px;
-        padding: 30px 50px;
-      }
-    }
-  }
-}
-
-
-
 </style>
